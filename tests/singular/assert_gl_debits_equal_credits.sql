419f9c24364b0587f4df9e52fcea79feb6a321da
harsh.parikh@kenexai.com <harsh.parikh@kenexai.com>
2026-07-10 10:20:15 +0000

    changes

--- tests/singular/assert_gl_debits_equal_credits.sql
+++
@@ -1,8 +0,0 @@
--- GL double-entry: total debits should equal total credits per journal
-select
-    journal_entry_id,
-    sum(debit_amount) as total_debits,
-    sum(credit_amount) as total_credits
-from {{ ref('fct_journal_entries') }}
-group by journal_entry_id
-having abs(sum(debit_amount) - sum(credit_amount)) > 0.01
